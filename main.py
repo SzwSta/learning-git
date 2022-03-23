93e022eda31b67e2a5fdaf4f5bf37f2a3e8603f7
Stanislaw Szwiec <szwiec.st@jacobs.com>
2022-03-23 11:43:11 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+numbers=[]
+for i in range(1,101):
+    if i%5==0:
+        numbers.append(i)
+cubes=[number**3 for number in numbers]
+print(f"Zadanie 2 - poczatek\npoczatkowa lista: {numbers}\nlista szescianow: {cubes}\nZadanie 2 - koniec\n")
