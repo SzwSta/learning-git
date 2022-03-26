76f6695aa51a60802198d21f03115a35aa2fe19b
SzwSta <81262397+SzwSta@users.noreply.github.com>
2022-03-26 10:16:01 +0100

    test commit

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ for i in range(1,101):
         numbers.append(i)
 cubes=[number**3 for number in numbers]
 print(f"Zadanie 2 - poczatek\npoczatkowa lista: {numbers}\nlista szescianow: {cubes}\nZadanie 2 - koniec\n")
+print("test")
